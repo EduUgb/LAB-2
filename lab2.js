2f133b31d68ff85ff2eb7f60c291eddd9019d961
EduUgb <carloseduardog3200@gmail.com>
2025-02-11 20:56:28 -0600

    arreglos finales

--- lab2.js
+++ lab2.js
@@ -1,22 +1,41 @@
 
 function cambiafondo(element) {
-    element.style.backgroundColor = '#61e7ff';
+    element.style.backgroundColor = '#e2fbff';
+    
 }
 
 function sevafondo(element) {
     element.style.backgroundColor = '';
 }
 
-function showImage(element) {
-    const img = element.querySelector('.hover-image');
-    if (img) {
-        img.style.display = 'block';
+function cambiarColor(divId, inputId) {
+    let div = document.getElementById(divId);
+    let color = document.getElementById(inputId).value;
+    div.style.background = color;
+    alert("El color de fondo ha sido cambiado a: " + color);
+}
+function cambiaImagen(id) {
+    let div = document.getElementById(id);
+    div.classList.toggle("atras");
+}
+
+function textog(Idtext){
+    let div = document.getElementById(Idtext);
+    div.style.fontVariant = 'small-caps';
+}
+
+function textop(Idtext){
+    let div = document.getElementById(Idtext);
+    div.style.fontVariant = '';
+}
+
+function tecla(event){
+    if(event.key == "g" || event.keyCode == "G"){
+        const div = document.getElementById("teclag"); 
+        div.classList.toggle("gato");
     }
 }
 
-function hideImage(element) {
-    const img = element.querySelector('.hover-image');
-    if (img) {
-        img.style.display = 'none';
-    }
+function aviso(id){
+    alert("Este es mi talento");
 }
